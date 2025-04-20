999ef9d5977c3a87b29fa924e91496f8e8f2dc23
Mohamed <m.elsayed.a@gmail.com>
2025-04-20 21:39:09 +0000

    add: docker image build script tags

--- build.sh
+++ build.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+tag=
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -t)
+      tag="$2"
+      shift 2
+      ;;
+    *)
+      echo "Invalid option: $1" >&2
+      usage
+      ;;
+  esac
+done
+
+echo "tag => $tag"
+
 export LLVM_LIB_CFG_PATH=${LLVM_LIB_CFG_PATH:-/usr/lib/llvm-16/lib}
 cargo build --release
 cp target/release/pagezest docker
@@ -15,3 +31,7 @@ cp plugins/*.wasm docker/plugins/
 cp plugins/*.json docker/plugins/
 
 docker build -t pagezest docker
+
+docker push pagezest/pagezest
+docker image tag pagezest/pagezest pagezest/pagezest:v0.1
+docker image push --all-tags pagezest/pagezest
