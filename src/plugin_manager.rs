5dc684ca50df976bd2ae2d70b861dd0c0f4a637b
Mohamed <m.elsayed.a@gmail.com>
2025-04-27 15:45:44 +0000

    add: PluginManager

---
+++ src/plugin_manager.rs
@@ -0,0 +1,173 @@
+use std::{cell::RefCell, collections::HashMap, error::Error, fs, path::{Path, PathBuf}, rc::Rc};
+
+use serde_json::Value;
+use wasmi::{Caller, Config, Engine, Func, Instance, Linker, Memory, Module, Store, TypedFunc};
+
+use crate::errors::AppError;
+
+#[derive(Debug)]
+pub struct Plugin {
+    name: String,
+    tag: String,
+    wasm_file_path: String,
+    plugin_func_name: String,
+    instance: Instance,
+    memory: Memory,
+    store: Store<()>,
+    plugin_func: TypedFunc<(u32, u32), u32>,
+}
+
+#[derive(Debug)]
+pub struct PluginManager {
+    plugins: HashMap<String, Rc<RefCell<Plugin>>>
+}
+
+impl PluginManager {
+   pub fn new() -> Self {
+        Self { plugins: HashMap::new() }
+   }
+
+   pub fn load_plugin(&mut self, plugin: Plugin) {
+       //let plugin = Plugin::new(name, tag);
+       self.plugins.insert(plugin.tag.to_string(), Rc::new(RefCell::new(plugin)));
+   }
+
+   pub fn unload_plugin(&mut self, tag: &str) {
+       self.plugins.remove(tag);
+   }
+
+   pub fn list_plugins(self) {
+       self.plugins.keys();
+   }
+
+   pub fn has_plugin_handler(&mut self, tag: &str) -> bool {
+       self.plugins.contains_key(tag)
+   }
+
+   pub fn get_plugin_by_tag(&mut self, tag: &str) -> Result<&Rc<RefCell<Plugin>>, AppError> {
+       match self.plugins.get(tag) {
+           Some(plugin) => {
+               Ok(plugin)
+           },
+           _ => {
+               Err(AppError::PluginError("Not found".to_string()))
+           }
+       }
+
+   }
+
+   pub fn load_plugin_dir(&mut self, path: PathBuf) -> Result<(), Box<dyn Error>> {
+       if path.is_dir() {
+            let manifest_path = path.join("manifest.json");
+            if manifest_path.exists() {
+                match fs::read_to_string(manifest_path) {
+                    Ok(manifest_content) => {
+                        let json: Value = serde_json::from_str(&manifest_content).expect("could not parse manifest");
+                        let name = json.get("name").expect("Manifest: name not found").to_string();
+                        let tag = json.get("tag").expect("Manifest: tag not found {}").to_string();
+                        let wasm_path = json.get("wasm_path").and_then(|s| s.as_str()).expect("Manifest: wasm_path not found").to_string();
+                        let wasm_path_org = json.get("wasm_path").and_then(|s| s.as_str()).expect("Manifest: wasm_path not found").to_string();
+                        let plugin_func_name = json.get("func_name").and_then(|s| s.as_str()).expect("Manifest: func_name not found").to_string();
+                        let wasm_path = path.join(wasm_path);
+                        let wasm_path_str = wasm_path.to_str().unwrap();
+                        match Plugin::new(&name, &tag, wasm_path_str, &plugin_func_name) {
+                            Ok(plugin) => {
+                                self.load_plugin(plugin);
+                            },
+                            Err(e) => {
+                                return Err(e.into());
+                            }
+                        }
+                    },
+                    Err(e) => {
+                        return Err(Box::new(e));
+                    }
+                }
+            }
+       }
+       Ok(())
+   }
+
+   pub fn scan_plugins(&mut self) -> Result<(), AppError> {
+       let path = Path::new("plugins");
+       if path.is_dir() {
+            if let Ok(entries) = fs::read_dir(path) {
+                for entry in entries.filter_map(Result::ok) {
+                    let entry_path = entry.path();
+                    self.load_plugin_dir(entry_path).expect("Error loading plugin");
+                }
+            }
+       }
+       Ok(())
+   }
+}
+
+impl Plugin {
+    pub fn new(name: &str, tag: &str, wasm_path: &str, plugin_func_name: &str) -> Result<Self, Box<dyn Error>> {
+        let wasm = std::fs::read(wasm_path)?;
+        let engine = Engine::new(&Config::default());
+        let module = Module::new(&engine, &wasm)?;
+
+        let mut store = Store::new(&engine, ());
+        let mut linker = Linker::new(&engine);
+        let abort_func = Func::wrap(&mut store, abort_stub);
+        linker.define("env", "abort", abort_func)?;
+        let instance = linker.instantiate(&mut store, &module)?.start(&mut store)?;
+
+        let memory = instance
+            .get_export(&store, "memory")
+            .and_then(|ext| ext.into_memory()).unwrap();
+        let plugin_func =
+            instance.get_typed_func(&store, &plugin_func_name.to_string())?;
+        Ok(
+            Self {
+                name: name.to_string(),
+                tag: tag.to_string(),
+                wasm_file_path: wasm_path.to_string(),
+                plugin_func_name: "".to_string(),
+                plugin_func,
+                memory,
+                instance,
+                store,
+            }
+        )
+    }
+
+
+    pub fn call(
+        &mut self, input: &str
+    ) -> Result<String, Box<dyn Error>> {
+        let offset = 0u32;
+        let res_ptr = (self.plugin_func)
+            .call(&mut self.store, (offset, input.len() as u32))?;
+        let mut output = Vec::new();
+        let mut curr_ptr = res_ptr;
+        loop {
+            let mut buf = [0u8, 1];
+            self.memory
+                .read(&mut self.store, curr_ptr as usize, &mut buf)
+                .unwrap();
+            if buf[0] == 0 {
+                break;
+            }
+            output.push(buf[0]);
+            curr_ptr += 1;
+        }
+
+        let output_str = String::from_utf8(output).unwrap();
+        Ok(output_str)
+
+    }
+}
+
+fn abort_stub(_caller: Caller<'_, ()>, _msg_ptr: i32, _file_ptr: i32, _line: i32, _col: i32) {}
+
+
+#[cfg(test)]
+
+#[test]
+fn test_plugin_manager() {
+    let mut m = PluginManager::new();
+    //m.load_plugin("a", "a");
+    m.unload_plugin("a");
+}
