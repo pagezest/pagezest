3874eca86a3aa9bc5b3bad869e0dead93ca3743f
Alex Guo <chessnut@outlook.com>
2025-04-28 07:45:50 -0500

    automation for testing ui

---
+++ scripts/test.sh
@@ -0,0 +1,23 @@
+# this script is used to run tests
+
+set -x  # print commands
+set -e  # exit on error
+
+export TOP="$PWD"
+
+test_ui() {
+  cd $TOP/admin
+  npm install
+  npm run build
+  cd $TOP
+  rm -rf $TOP/target/debug/pz-admin
+  cp -r $TOP/admin/dist $TOP/target/debug/pz-admin
+}
+
+main() {
+  case "$1" in
+    ui) test_ui ;;
+  esac
+}
+
+main $@
