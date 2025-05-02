fc7e19b674e16c3764d85c473454df2bcdfa0ceb
Mohamed <m.elsayed.a@gmail.com>
2025-05-02 17:41:59 +0000

    improve: TOC zig plugin to use wasm_allocator

--- plugins/toc/src/toc.zig
+++ plugins/toc/src/toc.zig
@@ -1,52 +1,45 @@
 const std = @import("std");
-extern "console.log" fn log(message: [*:0]const u8) void;
+extern "console.log" fn log(message: [*]const u8) void;
 
-var return_buffer: [65536]u8 = undefined;
-
-fn append_to_str(src: []u8, offset: usize, s: []const u8) usize {
-    std.mem.copyForwards(u8, src[offset..s.len], s);
-    return offset + s.len;
-}
-
-export fn toc(ptr: [*]const u8, len: usize) [*:0]const u8 {
+export fn toc(ptr: [*]const u8, len: usize) usize {
     const input = ptr[0..len];
 
-    var fba = std.heap.FixedBufferAllocator.init(&return_buffer);
-    const allocator = fba.allocator();
+    const allocator = std.heap.wasm_allocator;
+
     const parsed = std.json.parseFromSlice(std.json.Value, allocator, input, .{}) catch {
-        return "parse error";
+        return @intFromPtr("parse error".ptr);
     };
 
     const inputObj = parsed.value;
 
     if (inputObj != .object)
-        return "not an object";
+        return @intFromPtr("not an object".ptr);
 
     const obj = inputObj.object;
-    const root = obj.get("root") orelse return "no root";
+    const root = obj.get("root") orelse return @intFromPtr("no root".ptr);
 
     if (root != .array)
-        return "not a array";
+        return @intFromPtr("not a array");
 
     const children = root.array;
-    var offset: u32 = 0;
 
-    offset = append_to_str(&return_buffer, offset, "<h1>TOC</h1>");
-    offset = append_to_str(&return_buffer, offset, "<ul>");
+    var output = std.ArrayList(u8).init(allocator);
+    var writer = output.writer();
+    _ = writer.print("<h1>TOC</h1>", .{}) catch null;
+    _ = writer.print("<ul>", .{}) catch null;
     for (children.items) |c| {
         const typ = c.object.get("type").?.string;
         const depth = c.object.get("depth").?.integer;
-        if (std.mem.eql(u8, typ, "heading") and depth == 1) {
-            const text = c.object.get("text").?.string;
-            offset = append_to_str(&return_buffer, offset, "<li>");
-            offset = append_to_str(&return_buffer, offset, text);
-            offset = append_to_str(&return_buffer, offset, "</li>");
+        if (depth == 1) {
+            _ = writer.print("<li style='margin-left: {d}px'>", .{depth}) catch null;
+            _ = writer.print("{s}", .{typ}) catch null;
+            _ = writer.print("</li>", .{}) catch null;
         }
     }
-    offset = append_to_str(&return_buffer, offset, "</ul>");
-    offset = append_to_str(&return_buffer, offset, "<hr/>");
+    _ = writer.print("</ul>", .{}) catch null;
 
-    return_buffer[offset] = 0;
+    _ = writer.writeByte(0) catch null;
 
-    return return_buffer[0..offset :0];
+    const slice = output.toOwnedSlice() catch "err";
+    return @intFromPtr(slice.ptr);
 }
