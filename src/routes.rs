7e27743ba0a16cbef388d376953202b621bc587d
Mohamed <m.elsayed.a@gmail.com>
2025-05-04 11:38:59 +0000

    add: UI serve_embed or static files handler
    Signed-off-by: Mohamed <m.elsayed.a@gmail.com>

--- src/routes.rs
+++ src/routes.rs
@@ -10,7 +10,20 @@ pub fn config(cfg: &mut web::ServiceConfig) {
     .route("/api/blog/{id}", web::put().to(api::update_blog_post))
     .route("/api/blog/{id}", web::delete().to(api::delete_blog_post))
     .route("/api/blog{trailing:/?}", web::get().to(api::get_all_blog_posts))
-    .route("/api/stats", web::get().to(api::get_server_stats))
-    .route("/{tail:.*}", web::get().to(api::get_post_by_slug))
+    .route("/api/stats", web::get().to(api::get_server_stats));
+    serve_static_cfg(cfg);
+    cfg.route("/api/preview/{tail:.*}", web::get().to(api::get_post_by_slug));
+    cfg.route("/{tail:.*}", web::get().to(api::get_post_by_slug));
+}
+
+#[cfg(not(feature = "embed_admin_ui"))]
+pub fn serve_static_cfg(cfg: &mut web::ServiceConfig) {
+  println!("Serve static::file system");
+  cfg
     .service(Files::new("/pz-admin", "./pz-admin").show_files_listing().redirect_to_slash_directory());
+}
+
+#[cfg(feature = "embed_admin_ui")]
+pub fn serve_static_cfg(cfg: &mut web::ServiceConfig) {
+    cfg.service(web::resource("/pz-admin{tail:.*}").to(api::serve_embedded));
 }
