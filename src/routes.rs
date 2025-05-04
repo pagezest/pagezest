cd011d5a51548422ded2605d8bed8b0bdbf5ef56
Mohamed <m.elsayed.a@gmail.com>
2025-05-04 09:12:43 +0000

    add: actix routes
    Signed-off-by: Mohamed <m.elsayed.a@gmail.com>

---
+++ src/routes.rs
@@ -0,0 +1,16 @@
+use actix_files::Files;
+use actix_web::web;
+
+use crate::api;
+
+pub fn config(cfg: &mut web::ServiceConfig) {
+  cfg
+    .route("/api/blog/", web::post().to(api::create_new_blog_post))
+    .route("/api/blog/{id}", web::get().to(api::find_blog_by_id))
+    .route("/api/blog/{id}", web::put().to(api::update_blog_post))
+    .route("/api/blog/", web::get().to(api::get_all_blog_posts))
+    .route("/api/blog/{id}", web::delete().to(api::delete_blog_post))
+    .route("/api/stats", web::get().to(api::get_server_stats))
+    .route("/{tail:.*}", web::get().to(api::get_post_by_slug))
+    .service(Files::new("/pz-admin", "./pz-admin").show_files_listing().redirect_to_slash_directory());
+}
