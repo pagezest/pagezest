3e87da0f5c233b190f5857cabdf08ca0f3a12967
Mohamed <m.elsayed.a@gmail.com>
2025-05-07 21:15:02 +0000

    fix: vite, load env files

--- admin/vite.config.ts
+++ admin/vite.config.ts
@@ -1,28 +1,34 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
+
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
+export default ({mode}) => {
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+  return defineConfig({
+    publicDir: 'pz-admin',
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+      },
     },
-  },
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-  base: process.env.VITE_BASE_URL || './',
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:8080',
-        changeOrigin: true,
-        //rewrite: path => path.replace(/^\/api/, '')
+    optimizeDeps: {
+      exclude: ['lucide-react'],
+    },
+    base: process.env.VITE_BASE_URL || './',
+    server: {
+      proxy: {
+        '/api': {
+          target: 'http://127.0.0.1:8080',
+          changeOrigin: true,
+          //rewrite: path => path.replace(/^\/api/, '')
+        }
       }
+      /*
+       */
     }
-    /*
-    */
-  }
-});
+  });
+};
