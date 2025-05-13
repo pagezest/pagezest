fbd0ee52aea1495f1a90010073d59d991667f88d
Mohamed <m.elsayed.a@gmail.com>
2025-05-13 20:03:24 +0000

    add: post.rs add ID field and to_json

--- src/post.rs
+++ src/post.rs
@@ -1,27 +1,42 @@
 use chrono::Utc;
 use serde::{Deserialize, Serialize};
-use serde_json::Value;
+use serde_json::{json, Value};
 
-#[derive(Debug, Serialize, Deserialize)]
+use crate::uuid::generate_uuid;
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct BlogPost {
+    pub id: String,
     pub title: String,
-    pub content: Value,
     #[serde(skip)]
     pub content_flatbuffer: Vec<u8>,
+    pub content_md: String,
     pub slug: String,
     pub created_at: String,
     pub updated_at: String,
 }
 
 impl BlogPost {
-    pub fn new(slug: &str, title: &str, content: Value, content_flatbuffer: Vec<u8>) -> BlogPost {
+    pub fn new(slug: &str, title: &str, content_md: &str, content_flatbuffer: Vec<u8>) -> BlogPost {
         Self {
+            id: generate_uuid().to_string(),
             title: title.to_string(),
-            content,
+            content_md: content_md.to_string(),
             content_flatbuffer,
             slug: slug.to_string(),
             created_at: Utc::now().to_string(),
             updated_at: Utc::now().to_string(),
         }
+    }
+
+    pub fn to_json(&self) -> Value {
+        json!({
+            "id": self.id,
+            "slug": self.slug,
+            "title": self.title,
+            "content_md": self.content_md,
+            "created_at": self.created_at,
+            "updated_at": self.updated_at,
+        })
     }
 }
